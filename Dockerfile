6bc3c3395508a320a81aa03c5a7af552ec31f55d
ajitnarayanan <ajit.narayanan@gmail.com>
2024-06-25 22:27:49 +0530

    changed app things"

--- Dockerfile
+++ Dockerfile
@@ -27,5 +27,5 @@ RUN ls
 RUN addgroup -g 10016 choreo && \
     adduser  --disabled-password  --no-create-home --uid 10016 --ingroup choreo choreouser
 USER 10016
-EXPOSE 9090
+EXPOSE 5000
 CMD [ "flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -3,6 +3,18 @@ app = Flask(__name__)
 
 
 
-@app.route("/scraper/scrape") 
+@app.route("/scraper/scrape",methods=['GET']) 
 def home():
     return "Hello, Flask!"
+
+
+
+@app.route('/scraper/healthCheck', methods=['GET'])
+def health_check():
+    return "Hello, Welcome"
+
+
+
+
+if __name__ == "__main__":
+    app.run()
